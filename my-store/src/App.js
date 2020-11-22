041441e7e7dafea83ec5712f63abe73d5543c789
maurya512 <patelmaurya0512@gmail.com>
2020-11-22 18:22:21 -0500

    created and added a header component

---
+++ my-store/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import the header tag from Header.js
+import Header from './Header';
+
+function App() {
+  return (
+    // bem convention
+    <div className="app">
+      {/* <h1> Maurya's online store</h1> */}
+      {/* Header  */}
+      <Header/>
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
