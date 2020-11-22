c142e05921d099f8e11ee82a29dbd27b369cd7a0
maurya512 <patelmaurya0512@gmail.com>
2020-11-22 18:25:16 -0500

    firebase config files

---
+++ my-store/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCXvFuvBUxgahVsURKACXGuy9n35GmiCj4",
+    authDomain: "my-store-ce964.firebaseapp.com",
+    databaseURL: "https://my-store-ce964.firebaseio.com",
+    projectId: "my-store-ce964",
+    storageBucket: "my-store-ce964.appspot.com",
+    messagingSenderId: "796436775552",
+    appId: "1:796436775552:web:24ab0dcc0e627f6e755c65",
+    measurementId: "G-V6HRYNVDJ2"
+  };  
