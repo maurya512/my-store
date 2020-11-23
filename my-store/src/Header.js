72021681f355aa5e02c0d02b0f011609a5043e94
maurya512 <patelmaurya0512@gmail.com>
2020-11-22 22:48:47 -0500

    Header component now supports link to and from checkout page

--- my-store/src/Header.js
+++ my-store/src/Header.js
@@ -3,22 +3,25 @@ import React from 'react'
 // import the searchIcon from materialui
 import SearchIcon from '@material-ui/icons/Search';
 // import the shoppig basket icon from material ui
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';  
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <div className="header "> 
-             <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
-             <div className="header__search">
-                 <input className="header__searchInput" type="text" />
-                 <SearchIcon className="header__searchIcon"/>
-                 
-                 {/* Logo */}
-             </div>
+        <div className="header ">
+            <Link to="/">
+                <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt="store logo"/>
+            </Link>
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
 
-             <div className="header__nav">
-                 {/* options are the ones that have hello sign in, cart and such */}
+                {/* Logo */}
+            </div>
+
+            <div className="header__nav">
+                {/* options are the ones that have hello sign in, cart and such */}
                 <div className="header__option">
                     <span className="header__optionLineOne">Hello Guest</span>
                     <span className="header__optionLineTwo">Sign In</span>
@@ -28,15 +31,18 @@ function Header() {
                     <span className="header__optionLineTwo">Orders</span>
                 </div>
                 <div className="header__option">
-                <span className="header__optionLineOne">Your</span>
-                <span className="header__optionLineTwo">Prime</span>
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
                 </div>
                 {/* to add a cart logo for the  */}
-                <div className="header__optionBasket">
-                  <ShoppingBasketIcon />
-                    <span className="header__optionLineTwo header__basketCount" >0 </span>
+                {/* add link to the shopping cart page */}
+                <Link to='/checkout'>
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount" >0 </span>
                     </div>
-             </div>
+                </Link>
+            </div>
         </div>
     )
 }
