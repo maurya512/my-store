41b090df23bc7e0bb3649b331d4bd019ac3ad3e6
maurya512 <patelmaurya0512@gmail.com>
2020-11-22 21:15:44 -0500

    Added default and checkout routes

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -4,16 +4,29 @@ import './App.css';
 import Header from './Header';
 // import the home tag from Home.js
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
  
 function App() {
   return (
     // bem convention
+    <Router>
     <div className="app">
-      {/* <h1> Maurya's online store</h1> */}
-      {/* Header  */}
-      <Header/>
-      < Home />
+      <Switch>
+        {/* checkout path  */}
+        <Route path='/checkout'>
+          <Header />
+          <h1>I AM A CHECK OUT PAGE</h1>
+        </Route>
+        {/* default route always stays at the bottom as that's the last case scenario */}
+        {/* a default route when a designated path isn't hit */}
+        {/* Home page route */}
+        <Route path='/'>
+        <Header/>
+        <Home />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
