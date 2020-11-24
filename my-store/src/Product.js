2479040e328fd0e482e4e88a24ef7e8a497a6c83
maurya512 <patelmaurya0512@gmail.com>
2020-11-23 23:00:43 -0500

    implemented a way to dispatch items into dataLayer using context api and props

--- my-store/src/Product.js
+++ my-store/src/Product.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import './Product.css';
+import { useStateValue } from './StateProvider';
 // props
 function Product({id, title, image, price, rating}) {
+    const [{ cart }, dispatch] = useStateValue();
+    console.log('this is the cart: ' + cart);
+    const addToCart = () => {
+        // dispatch the item to dataLayer
+        // how we manipulate the data
+        dispatch({
+            // when add to cart
+            type: 'Add__To__Cart',
+            // items to be pushed into the data/layer are the id, title, image, price and ratings are the ones passed in from the props
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
     return (
         <div className="product">
             {/* info about the product */}
@@ -19,8 +38,8 @@ function Product({id, title, image, price, rating}) {
                 </div>
             </div>
             <img src={image} alt="lean startup book" />
-
-            <button>Add to cart</button>
+            {/* added an onClick to button */}
+            <button onClick={addToCart}>Add to cart</button>
         </div>
     )
 }
