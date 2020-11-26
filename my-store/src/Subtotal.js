32a63eb99905e65e54030113c7d7e314e553407b
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 16:47:52 -0500

    total is comma separated and 2 decimal places

--- my-store/src/Subtotal.js
+++ my-store/src/Subtotal.js
@@ -7,31 +7,31 @@ import { getCartTotal } from './reducer';
 function Subtotal() {
     const [{ cart }, dispatch] = useStateValue();
     return (
-        <div className="subtotal"> 
+        <div className="subtotal">
             <CurrencyFormat
-            renderText={(value) => (
-                <>
-                <p>
-                    {/* pulling from the cart and displaying the number of items in the cart */}
+                renderText={(value) => (
+                    <>
+                        <p>
+                            {/* pulling from the cart and displaying the number of items in the cart */}
             Subtotal ({cart.length} items): <strong>{value}</strong>
-                </p>
-                <small className="subtotal__gift">
-                    <input type="checkbox" />
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" />
                     This order contains a gift
                 </small>
-                </>
-            )}
-            // goes to 2 decimal places
-            decimalScale={2}
-            // amount getting passed
-            value={getCartTotal(cart)}
-            displayType={"text"}
-            // separates the currency into readable format
-            thousandSeparator={true}
-            prefix={"$"}
+                    </>
+                )}
+                // goes to 2 decimal places
+                decimalScale={2}
+                // amount getting passed
+                value={getCartTotal(cart)}
+                displayType={"text"}
+                // separates the currency into readable format
+                thousandSeparator={true}
+                prefix={"$"}
             />
             <button>Proceed to checkout</button>
-        </div> 
+        </div>
     );
 }
 
