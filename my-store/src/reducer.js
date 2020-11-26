375e42938ec133c1390ed56ab2eb2ee10c085d00
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 13:16:45 -0500

    created a selector that calculates the subtotal of the items by looping through each item in the cart

--- my-store/src/reducer.js
+++ my-store/src/reducer.js
@@ -2,6 +2,13 @@ export const initialState = {
     cart: [],
 };
 
+// selector
+export const getCartTotal = (cart)=> {
+    // reduce iterates through the cart and will tally the amount
+    // tally's the cart and tally's the price of each added item into the cart and adds it to the total amount, and sets the total amount to 0 initially
+    cart?.reduce((amount, item) => item.price + amount, 0);
+}
+
 // takes the state of the application and the action we want to perform
 const reducer = (state, action) => {
     console.log(action);
