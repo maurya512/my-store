6dc2ee15d77c9403d7bfd57697a0edcb63426bb1
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 18:15:06 -0500

    removes an item from the cart

--- my-store/src/reducer.js
+++ my-store/src/reducer.js
@@ -20,6 +20,31 @@ const reducer = (state, action) => {
                 ...state,
                 cart: [...state.cart, action.item]
             };
+        
+        case 'Remove__From__Cart':
+            // since reducer is always listening for something to dispatch something to/from the dataLayer
+            // goes through all of the items in the cart and finds the first item with the id of the item
+            const index = state.cart.findIndex(
+                (cartItem) => cartItem.id === action.id
+            );
+            // create a newCart that has the current items in the cart
+            let newCart = [...state.cart];
+
+            // check if the index is >= 0 which means that the item matching was found
+            if (index >= 0) {
+                // go to the new Cart and splice and delete the element with the found id
+                newCart.splice(index, 1);
+            }
+              
+            // else give an error message to the world
+            else {
+                console.warn(`Can't remove product (id: ${action.id}) as its not in cart`);
+            }
+
+            return {
+             ...state,
+             cart: newCart   
+            }
 
             default:
                 return state;
