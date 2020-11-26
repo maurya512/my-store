829b2117cacab76b754417dfad97f7e6d6a26864
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 13:15:20 -0500

    subtotal fetches the number of items in the cart and displays on the checkout page

--- my-store/src/Subtotal.js
+++ my-store/src/Subtotal.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from './StateProvider';
+import { getCartTotal } from './reducer';
+
 function Subtotal() {
+    const [{ cart }, dispatch] = useStateValue();
     return (
         <div className="subtotal"> 
             <CurrencyFormat
             renderText={(value) => (
                 <>
                 <p>
-                    Subtotal (0 items): <strong>0</strong>
+                    {/* pulling from the cart and displaying the number of items in the cart */}
+            Subtotal ({cart.length} items): <strong>{value}</strong>
                 </p>
                 <small className="subtotal__gift">
                     <input type="checkbox" />
@@ -19,7 +24,7 @@ function Subtotal() {
             // goes to 2 decimal places
             decimalScale={2}
             // amount getting passed
-            value={0}
+            value={getCartTotal(cart)}
             displayType={"text"}
             // separates the currency into readable format
             thousandSeparator={true}
