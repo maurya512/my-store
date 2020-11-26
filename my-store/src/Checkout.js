08556ebfb3e6cf4dc8c078995203ef7dadca1c39
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 16:47:22 -0500

    checkout page now shows total and the pictures of items in the cart

--- my-store/src/Checkout.js
+++ my-store/src/Checkout.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './Checkout.css';
 import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 function Checkout() {
+
+    // pulls the cart from the dataLayer
+    const [{ cart }, dispatch] = useStateValue();
     return (
         <div className="checkout">
             {/* have two sections for the checkout page */}
@@ -11,8 +16,16 @@ function Checkout() {
                 <div>
                     <h2 className="checkout__title">
                         Your Shopping Cart
-                        {/* Shopping Cart */}
                     </h2>
+                    {cart.map(item => (
+                        <CheckoutProduct 
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                    ))}
                 </div>
             </div>
             <div className="checkout__right">
