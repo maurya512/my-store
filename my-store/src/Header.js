510fdbdb17fa95fb76b7021ab92a7a3cb2d0bf88
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 22:01:53 -0500

    added logout capabilities

--- my-store/src/Header.js
+++ my-store/src/Header.js
@@ -7,13 +7,20 @@ import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
-    const [{cart}, dispatch] = useStateValue();
+    const [{ cart, user }, dispatch] = useStateValue();
+    const handleAuthentication = () => {
+        if (user) {
+            // pull the authentication module from firebase local
+            auth.signOut();
+        }
+    }
     return (
         <div className="header ">
             <Link to="/">
-                <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt="store logo"/>
+                <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt="store logo" />
             </Link>
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
@@ -23,12 +30,12 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <Link to='/login'>
-                {/* options are the ones that have hello sign in, cart and such */}
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello Guest</span>
-                    <span className="header__optionLineTwo">Sign In</span>
-                </div>
+                <Link to={!user && '/login'}>
+                    {/* options are the ones that have hello sign in, cart and such */}
+                    <div onClick={handleAuthentication}className="header__option">
+                        <span className="header__optionLineOne">Hello Guest</span>
+                        <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
+                    </div>
                 </Link>
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
