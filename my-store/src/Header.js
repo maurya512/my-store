1a9f2e3753d1672605b248ee2003bc2af1768dc2
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 21:16:01 -0500

    added routes and links to different components

--- my-store/src/Header.js
+++ my-store/src/Header.js
@@ -23,11 +23,13 @@ function Header() {
             </div>
 
             <div className="header__nav">
+                <Link to='/login'>
                 {/* options are the ones that have hello sign in, cart and such */}
                 <div className="header__option">
                     <span className="header__optionLineOne">Hello Guest</span>
                     <span className="header__optionLineTwo">Sign In</span>
                 </div>
+                </Link>
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
                     <span className="header__optionLineTwo">Orders</span>
