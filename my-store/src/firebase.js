cd6ea21108fc95ff920230ccfb10365e69d56fd4
maurya512 <patelmaurya0512@gmail.com>
2020-11-26 21:16:25 -0500

    added config files, setup user and database auth

--- my-store/src/firebase.js
+++ my-store/src/firebase.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCXvFuvBUxgahVsURKACXGuy9n35GmiCj4",
     authDomain: "my-store-ce964.firebaseapp.com",
@@ -9,3 +14,10 @@ const firebaseConfig = {
     appId: "1:796436775552:web:24ab0dcc0e627f6e755c65",
     measurementId: "G-V6HRYNVDJ2"
   };  
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
