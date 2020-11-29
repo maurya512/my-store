4fbb88fde96bad8b984e945adf75ffd9720b0254
maurya512 <patelmaurya0512@gmail.com>
2020-11-29 14:23:37 -0500

    removed a lingering text outside the page's scope

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -7,6 +7,7 @@ import Home from './Home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
+import Payment from './Payment';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
 function App() {
@@ -47,6 +48,10 @@ function App() {
           {/* checkout component */}
           <Header />
           <Checkout />
+        </Route>
+        <Route path='/payment'>
+          <Header />
+          <Payment />
         </Route>
         {/* default route always stays at the bottom as that's the last case scenario */}
         {/* a default route when a designated path isn't hit */}
