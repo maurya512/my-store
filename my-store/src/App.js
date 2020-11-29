8cd875497455ca7b2ef964a7321b3b0c56279bd9
maurya512 <patelmaurya0512@gmail.com>
2020-11-29 16:43:26 -0500

    added comments

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 import Header from './Header';
 // import the home tag from Home.js
 import Home from './Home';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
 import Payment from './Payment';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+// public key
+const promise = loadStripe('pk_test_51HswFsFCp7ibaXEOuCdMv73Xm2PRiKSrBf6alwEXlbPT0wYeeBLqMZRk42Lo2sengtQ9sBciYnrAPvFvYw3QhULZ00mRTGCyMx');
 function App() {
-  const [{}, dispatch] = useStateValue();
+  const [{ }, dispatch] = useStateValue();
   useEffect(() => {
     // will only run once the app component loads
     auth.onAuthStateChanged(authUser => {
       console.log('The user is ' + authUser);
-      if(authUser) {
+      if (authUser) {
         // the user just logged in
         dispatch({
           // set the user to whoever was logged in using firebase 
@@ -24,44 +29,46 @@ function App() {
           user: authUser
         })
       } else {
-      dispatch({
-        type: 'SET_USER',
-        user: null
-      })
-    }
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
     })
   }, [])
 
   return (
     // bem convention
     <Router>
-    <div className="app">
-      {/* header always gets rendered */}
-      <Switch>
-        {/* Login route */}
-        <Route path='/login'>
-          <Login />
-          {/* Login component */}
-        </Route>
-        {/* checkout path  */}
-        <Route path='/checkout'>
-          {/* checkout component */}
-          <Header />
-          <Checkout />
-        </Route>
-        <Route path='/payment'>
-          <Header />
-          <Payment />
-        </Route>
-        {/* default route always stays at the bottom as that's the last case scenario */}
-        {/* a default route when a designated path isn't hit */}
-        {/* Home page route */}
-        <Route path='/'>
-          <Header/>
-        <Home />
-        </Route>
-      </Switch>
-    </div>
+      <div className="app">
+        {/* header always gets rendered */}
+        <Switch>
+          {/* Login route */}
+          <Route path='/login'>
+            <Login />
+            {/* Login component */}
+          </Route>
+          {/* checkout path  */}
+          <Route path='/checkout'>
+            {/* checkout component */}
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path='/payment'>
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          {/* default route always stays at the bottom as that's the last case scenario */}
+          {/* a default route when a designated path isn't hit */}
+          {/* Home page route */}
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
